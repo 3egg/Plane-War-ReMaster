628da2a5ad525557258af18a9fcd3e973ab0db7f
3egg <627096931@qq.com>
2019-06-09 17:30:25 +0800

    second commit

---
+++ Assets/Script/GamePlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePlayerController : MonoBehaviour
+{
+    private void Awake()
+    {
+        int length = FindObjectsOfType<GamePlayerController>().Length;
+        if (length > 1)
+        {
+            DestroyObject(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void LoadGamePlayScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Script/MusicController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+     private AudioSource audioSource;
+
+    private void Awake()
+    {
+        /*
+        int length = FindObjectsOfType<MusicController>().Length;
+        if (length > 1)
+        {
+            DestroyObject(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+*/
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayOrStopMusic()
+    {
+        //audioSource.enabled = true; // I put this just in case.
+        audioSource.Play(); // doesn't tell me that AudioSource is enabled anymore because of the previous line, but still does not play.
+    }
+}
